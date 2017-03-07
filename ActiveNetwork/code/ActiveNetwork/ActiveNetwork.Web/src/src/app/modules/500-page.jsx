fff121a26b7a0d26307ab1390496bd53b1167612
linkin6713 <hoangvhh@gmail.com>
2017-03-07 17:56:42 +0700

    + Add error 500 page

--- ActiveNetwork/code/ActiveNetwork/ActiveNetwork.Web/src/src/app/modules/500-page.jsx
+++ ActiveNetwork/code/ActiveNetwork/ActiveNetwork.Web/src/src/app/modules/500-page.jsx
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 
-export class Error404Page extends Component {
+export class Error500Page extends Component {
   render() {
     return (<div>
 
       <div className="error-header"> </div>
       <div className="container ">
         <section className="error-container text-center">
-          <h1 className="animated fadeInDown">404</h1>
+          <h1 className="animated fadeInDown">500</h1>
           <div className="error-divider animated fadeInUp">
-            <h2>PAGE NOT FOUND.</h2>
-            <p className="description">We Couldn't Find This Page</p>
+            <h2>ooops!!</h2>
+            <p className="description">SOMETHING WENT WRONG.</p>
           </div>
           <a href="/" className="return-btn"><i className="fa fa-home"></i>Home</a>
         </section>
