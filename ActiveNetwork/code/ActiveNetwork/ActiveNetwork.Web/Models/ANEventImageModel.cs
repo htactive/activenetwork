c813b25e43f12cdfc48d88fcd0878210b7e4bfa2
linkin6713 <hoangvhh@gmail.com>
2017-03-11 12:09:31 +0700

    + Add Event Image Model

---
+++ ActiveNetwork/code/ActiveNetwork/ActiveNetwork.Web/Models/ANEventImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActiveNetwork.Web.Models
+{
+    public class ANEventMemberModel
+    {
+        public int Id { get; set; }
+        public int ANEventId { get; set; }
+        public int ImageId { get; set; }
+    }
+}
